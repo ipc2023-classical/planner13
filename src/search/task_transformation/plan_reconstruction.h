f5496336d8f4683ea9ec209d61f31899e2159f2d
Alvaro Torralba <torralba@cs.uni-saarland.de>
2018-10-18 19:04:26 +0200

    Commenting out stuff in plan reconstruction

--- src/search/task_transformation/plan_reconstruction.h
+++ src/search/task_transformation/plan_reconstruction.h
@@ -37,26 +37,26 @@ class PlanReconstructionStep : public PlanReconstruction {
     // We do plan reconstruction by performing a search on the predecessor task. We allow two type of transitions:
     // 1) Transitions with a label l that maps to the next label in the plan and whose target maps to the next abstract state in the plan
     // 2) Transitions with a tau label whose target maps to the same abstract state in the plan. 
-    std::shared_ptr<task_representation::FTSTask> predecessor_task;
+    //std::shared_ptr<task_representation::FTSTask> predecessor_task;
 
     // We need the merge and shrink representation, which for evety state in the
     // predecessor task, it obtains the state in the task where the plan was found.    
-    std::vector<MergeAndShrinkRepresentation> merge_and_shrink_representation;
+    //std::vector<MergeAndShrinkRepresentation> merge_and_shrink_representation;
 
     // We need a LabelMap from labels in the predecessor_task to labels in the task where
     // the plan was found.
-    LabelMap label_map;    
+    //LabelMap label_map;    
 
     // This is the set of tau_labels, that we can use. 
-    std::vector<int> tau_labels;
+    //std::vector<int> tau_labels;
 
 
 public:
 
-    PlanReconstructionStep (std::shared_ptr<task_representation::FTSTask> predecessor_task,
-			    std::vector<MergeAndShrinkRepresentation> merge_and_shrink_representation,
-			    LabelMap label_map,
-			    std::vector<int> tau_labels);
+    /* PlanReconstructionStep (std::shared_ptr<task_representation::FTSTask> predecessor_task, */
+    /* 			    std::vector<MergeAndShrinkRepresentation> merge_and_shrink_representation, */
+    /* 			    LabelMap label_map, */
+    /* 			    std::vector<int> tau_labels); */
     
     virtual void reconstruct_plan (std::vector<task_representation::LabelID> & plan,
 				   std::vector<task_representation::State> & traversed_states) const;
