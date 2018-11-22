1aeafb74fca3cf4ec1fc9d323029659f530a0a77
Silvan Sievers <silvan.sievers@unibas.ch>
2018-11-22 10:16:41 +0100

    merge paper tables

--- experiments/fts-search/paper-tables.py
+++ experiments/fts-search/paper-tables.py
@@ -23,8 +23,10 @@ from common_setup import IssueExperiment
 exp = FastDownwardExperiment()
 
 REVISIONS = [
-    'fts-search-base', # 11-05 baseline lazygreedy-ff*, baseline lama-first
-    '865a7642e263', # 11-20 lazygreedy-ff*
+    'fts-search-base', # 11-05 baseline astar-blind/hmax, baseline lama-first, baseline lazygreedy-ff*,
+    'fts-search-base-mas', # 11-15 baseline astar-mas
+    '01551daf787f', # 11-19 astar-blind/hmax/mas
+    '865a7642e263', # 11-21 lazygreedy-ff*
 ]
 
 def remove_revision(run):
@@ -34,6 +36,11 @@ def remove_revision(run):
     run['algorithm'] = algo
     return run
 
+exp.add_fetcher('data/2018-11-05-regular-baselines-eval',filter=[remove_revision])
+exp.add_fetcher('data/2018-11-15-baseline-mas-eval',filter=[remove_revision],merge=True)
+exp.add_fetcher('data/2018-11-19-astar-hmax-eval',filter=[remove_revision],merge=True)
+exp.add_fetcher('data/2018-11-19-astar-masmiasm-eval',filter=[remove_revision],merge=True)
+exp.add_fetcher('data/2018-11-19-astar-masdfp-eval',filter=[remove_revision],merge=True)
 exp.add_fetcher('data/2018-11-19-baseline-lazygreedy-ff-eval',filter=[remove_revision],merge=True)
 exp.add_fetcher('data/2018-11-19-baseline-lama-first-eval',filter=[remove_revision],merge=True)
 exp.add_fetcher('data/2018-11-21-lazygreedy-ff-eval',filter=[remove_revision],merge=True)
@@ -61,9 +68,67 @@ extra_attributes = [
 attributes = list(IssueExperiment.DEFAULT_TABLE_ATTRIBUTES)
 attributes.extend(extra_attributes)
 
+## Configs
+
+'astar-hmax',
+'astar-hmax-atomic',
+'astar-hmax-transform-atomic-labelreduction',
+'astar-hmax-transform-atomic-bisim-labelreduction',
+'astar-hmax-transform-full-bisim-labelreduction-dfp100-t900',
+'astar-hmax-transform-full-bisim-labelreduction-dfp1000-t900',
+'astar-hmax-transform-full-bisim-labelreduction-dfp10000-t900',
+'astar-hmax-transform-full-bisim-labelreduction-miasm100-t900',
+'astar-hmax-transform-full-bisim-labelreduction-miasm1000-t900',
+'astar-hmax-transform-full-bisim-labelreduction-miasm10000-t900',
+
+'astar-masdfpbisim50k',
+'astar-masdfpbisim50k-atomic',
+'astar-masdfpbisim50k-transform-atomic-labelreduction',
+'astar-masdfpbisim50k-transform-atomic-bisim-labelreduction',
+'astar-masdfpbisim50k-transform-full-bisim-labelreduction-dfp100-t900',
+'astar-masdfpbisim50k-transform-full-bisim-labelreduction-dfp1000-t900',
+'astar-masdfpbisim50k-transform-full-bisim-labelreduction-dfp10000-t900',
+'astar-masdfpbisim50k-transform-full-bisim-labelreduction-miasm100-t900',
+'astar-masdfpbisim50k-transform-full-bisim-labelreduction-miasm1000-t900',
+'astar-masdfpbisim50k-transform-full-bisim-labelreduction-miasm10000-t900',
+
+'astar-masmiasmbisim50k',
+'astar-masmiasmbisim50k-atomic',
+'astar-masmiasmbisim50k-transform-atomic-labelreduction',
+'astar-masmiasmbisim50k-transform-atomic-bisim-labelreduction',
+'astar-masmiasmbisim50k-transform-full-bisim-labelreduction-dfp100-t900',
+'astar-masmiasmbisim50k-transform-full-bisim-labelreduction-dfp1000-t900',
+'astar-masmiasmbisim50k-transform-full-bisim-labelreduction-dfp10000-t900',
+'astar-masmiasmbisim50k-transform-full-bisim-labelreduction-miasm100-t900',
+'astar-masmiasmbisim50k-transform-full-bisim-labelreduction-miasm1000-t900',
+
+'lazy-ff',
+'lazy-ff-atomic',
+'lazy-ff-transform-atomic-labelreduction',
+'lazy-ff-transform-atomic-bisimown-labelreduction',
+'lazy-ff-transform-full-bisimown-labelreduction-dfp100-t900',
+'lazy-ff-transform-full-bisimown-labelreduction-dfp1000-t900',
+'lazy-ff-transform-full-bisimown-labelreduction-dfp10000-t900',
+'lazy-ff-transform-full-bisimown-labelreduction-miasm100-t900',
+'lazy-ff-transform-full-bisimown-labelreduction-miasm1000-t900',
+'lazy-ff-transform-full-bisimown-labelreduction-miasm10000-t900',
+
+'lazy-ffpref',
+'lazy-ffpref-atomic',
+'lazy-ffpref-transform-atomic-labelreduction',
+'lazy-ffpref-transform-atomic-bisimown-labelreduction',
+'lazy-ffpref-transform-full-bisimown-labelreduction-dfp100-t900',
+'lazy-ffpref-transform-full-bisimown-labelreduction-dfp1000-t900',
+'lazy-ffpref-transform-full-bisimown-labelreduction-dfp10000-t900',
+'lazy-ffpref-transform-full-bisimown-labelreduction-miasm100-t900',
+'lazy-ffpref-transform-full-bisimown-labelreduction-miasm1000-t900',
+'lazy-ffpref-transform-full-bisimown-labelreduction-miasm10000-t900',
+
+'lama-first',
+
 ## HTML reports
 
-# exp.add_report(AbsoluteReport(attributes=['coverage']))
+exp.add_report(AbsoluteReport(attributes=['coverage']))
 
 ## Latex reports
 
