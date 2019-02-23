7e4ecc7bd1f4fbbaa06aee5e1e2b3365a19c584e
Silvan Sievers <silvan.sievers@unibas.ch>
2019-02-23 20:45:37 +0100

    run all experiments related to weak bisimulation: astar unit-cost blind, astar blind with weak bisimulation (all using unit-cost transformations, lazy greedy with ff (all using unit-cost transformations)

---
+++ experiments/fts-search/2019-02-23-astarunit-baseline.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import itertools
+import os
+
+from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+from lab.reports import Attribute, geometric_mean
+
+from downward.reports.compare import ComparativeReport
+
+from common_setup import IssueConfig, IssueExperiment, DEFAULT_OPTIMAL_SUITE, is_test_run
+
+REVISION = "fts-search-base-v2"
+
+def main(revisions=None):
+    benchmarks_dir=os.path.expanduser('~/repos/downward/benchmarks')
+    suite = DEFAULT_OPTIMAL_SUITE
+    environment = BaselSlurmEnvironment(email="silvan.sievers@unibas.ch", export=["PATH"], partition='infai_2')
+
+    if is_test_run():
+        suite = ['gripper:prob01.pddl', 'depot:p01.pddl', 'mystery:prob07.pddl']
+        environment = LocalEnvironment(processes=4)
+
+    configs = {
+        IssueConfig('astarunit-blind', ["--search", "astar(blind, cost_type=one)"]),
+    }
+
+    exp = IssueExperiment(
+        revisions=revisions,
+        configs=configs,
+        environment=environment,
+    )
+    exp.add_suite(benchmarks_dir, suite)
+
+    exp.add_parser(exp.EXITCODE_PARSER)
+    exp.add_parser(exp.TRANSLATOR_PARSER)
+    exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+    exp.add_parser(exp.PLANNER_PARSER)
+
+    attributes = exp.DEFAULT_TABLE_ATTRIBUTES
+
+    exp.add_step('build', exp.build)
+    exp.add_step('start', exp.start_runs)
+    exp.add_fetcher(name='fetch')
+
+    exp.add_absolute_report_step(attributes=attributes)
+
+    exp.run_steps()
+
+main(revisions=[REVISION])
