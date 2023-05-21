ec68da6698361b495624bffa1983c39e2ebbbca8
Silvan Sievers <silvan.sievers@unibas.ch>
2023-05-21 17:23:27 +0200

    remove duplicate variable

--- driver/aliases.py
+++ driver/aliases.py
@@ -154,8 +154,6 @@ ALIASES["seq-opt-fts-sbd"] = miasm1000_bisim + ["--search", "sbd()"]
 
 ALIASES["seq-opt-fts-ms"] =  miasm1000_bisim  + ["--search", "astar(merge_and_shrink(shrink_strategy=mas_shrink_bisimulation(greedy=false),merge_strategy=mas_merge_stateless(merge_selector=mas_score_based_filtering(scoring_functions=[mas_sf_miasm(shrink_strategy=mas_shrink_bisimulation(greedy=false),max_states=50000,threshold_before_merge=1),mas_total_order(atomic_ts_order=reverse_level,product_ts_order=new_to_old,atomic_before_product=false)])),label_reduction=mas_exact(before_shrinking=true,before_merging=false),max_states=50000,threshold_before_merge=1))"]
 
-miasm1000_bisim =  ["--transform", "transform_merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false),merge_strategy=merge_stateless(merge_selector=score_based_filtering(scoring_functions=[product_size(1000),sf_miasm(shrink_strategy=shrink_bisimulation(greedy=false),max_states=100,threshold_before_merge=1),total_order(atomic_ts_order=reverse_level,product_ts_order=new_to_old,atomic_before_product=false)])),label_reduction=exact(max_time=300,atomic_fts=true,before_shrinking=true,before_merging=false),shrink_atomic_fts=true,run_main_loop=true,max_time=900)"]
-
 ALIASES["seq-agl-fts-ff"] = ['--transform', 'transform_merge_and_shrink(shrink_strategy=shrink_weak_bisimulation(ignore_irrelevant_tau_groups=false),label_reduction=exact(max_time=300,atomic_fts=true,before_shrinking=true,before_merging=false),shrink_atomic_fts=true,run_main_loop=false,max_time=900,cost_type=one,prune_transitions_from_goal=true,prune_transitions_from_goal=true)', "--heuristic", "hff=ff(cost_type=one)", "--search", "lazy_greedy([hff], preferred=[hff], cost_type=one)"]
 
 
